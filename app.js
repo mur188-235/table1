746b0ac9b1bf3dc6840eab6bb22982a53c60fabb
mur188-235 <159010684+mur188-235@users.noreply.github.com>
2024-03-14 23:29:12 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+document.write("Muhammad Murtaza");
+var entrTable = prompt("Enter a number to generate it's table")
+
+for (i = 1; i < 11; i++) {
+console.log(entrTable , "x" ,i , "=" , entrTable*i);
+}
